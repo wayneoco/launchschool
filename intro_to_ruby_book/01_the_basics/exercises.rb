9861546b25ce936df298237b95f84fcd42641269
wayneoco <wayneoco@users.noreply.github.com>
2020-10-06 22:46:50 -0700

    Added Exercise 03

--- intro_to_ruby_book/01_the_basics/exercises.rb
+++ intro_to_ruby_book/01_the_basics/exercises.rb
@@ -1,5 +1,7 @@
+puts "Exercse 01"
 puts "Wayne" + " " + "Olson"
 
+puts "Exercise 02"
 thousands = 5678 / 1000
 puts "thousands: #{thousands}"
 
@@ -11,3 +13,14 @@ puts "tens: #{tens}"
 
 ones = 5678 % 10
 puts "ones: #{ones}"
+
+puts "Exercise 03"
+movies = {
+	"Goonies" => "1985",
+	"Karate Kid" => "1984",
+	"Last Samurai" => "2003"
+}
+
+movies.each do |name, year|
+	puts "#{year}"
+end
