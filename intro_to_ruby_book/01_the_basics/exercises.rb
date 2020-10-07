3ddb0d38a8e4e9ca6c48608313e7b8d1562c0154
wayneoco <wayneoco@users.noreply.github.com>
2020-10-06 23:05:04 -0700

    Added Exercises 05 & 06

--- intro_to_ruby_book/01_the_basics/exercises.rb
+++ intro_to_ruby_book/01_the_basics/exercises.rb
@@ -23,8 +23,63 @@ movies = {
 
 movies.each { |name, year| puts "#{year}" }
 
+puts "**LS's solution:"
+
+movies = {
+	:jaws => 1975,
+	:anchorman => 2004,
+	:man_of_steel => 2013,
+	:a_beautiful_mind => 2001,
+	:the_evil_dead => 1981
+	}
+	
+puts movies[:jaws]
+puts movies[:anchorman]
+puts movies[:man_of_steel]
+puts movies[:a_beautiful_mind]
+puts movies[:the_evil_dead]
+
+puts "LS's solution using update hash syntax as of Ruby 1.9:"
+
+movies = {
+	jaws: 1975,
+	anchorman: 2004,
+	man_of_steel: 2013,
+	a_beautiful_mind: 2001,
+	the_evil_dead: 1981
+}
+
+puts movies[:jaws]
+puts movies[:anchorman]
+puts movies[:man_of_steel]
+puts movies[:a_beautiful_mind]
+puts movies[:the_evil_dead]
+
 puts "Exercise 04"
 
 dates = ["1985", "1984", "2003"]
 
 dates.each { |year| puts "#{year}" }
+
+puts "LS's solution:"
+
+dates = [1975, 2004, 2013, 2001, 1981]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
+
+puts "Exercise 05"
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts "Exercise 06"
+
+puts 1.27**2
+puts 4.5678**2
+puts 8.142**2
