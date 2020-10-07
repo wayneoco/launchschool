bff70118a6478d99651daa8319d41e54345b1ef8
wayneoco <wayneoco@users.noreply.github.com>
2020-10-06 19:53:23 -0700

    Exercises 1 & 2

---
+++ 01.the_basics.rb
@@ -0,0 +1,13 @@
+"Wayne" + " " + "Olson"
+
+thousands = 5678 / 1000
+puts "thousands: #{thousands}"
+
+hundreds = 5678 % 1000 / 100
+puts "hundreds: #{hundreds}"
+
+tens = 5678 % 100 / 10
+puts "tens: #{tens}"
+
+ones = 5678 % 10
+puts "ones: #{ones}"
