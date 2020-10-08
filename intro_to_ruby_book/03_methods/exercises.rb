336adbf0dba6ac74288226d5ac3ddf1dcd4bc4e0
wayneoco <wayne@wayneo.co>
2020-10-08 14:06:35 +0100

    Added exercises to complete Chapter 03.

---
+++ intro_to_ruby_book/03_methods/exercises.rb
@@ -0,0 +1,25 @@
+# exercises.rb
+
+# Exercise 01
+
+def greeting(name)
+	puts "Hi, #{name}! Nice to meet you!"
+end
+
+puts greeting("Wayne")
+
+# Exercise 03
+
+def multiply(a,b)
+	a * b
+end
+
+p multiply(2,5)
+
+# Exercise 05
+
+def scream(words)
+	words = words + "!!!!"
+end
+
+p scream("Yippeee")
