41bbc0229e7a3e218b784af4de49f6e158ac93fa
wayneoco <wayneoco@users.noreply.github.com>
2020-10-08 05:02:07 -0700

    Added no_mutate exercise

--- intro_to_ruby_book/03_methods/mutate.rb
+++ intro_to_ruby_book/03_methods/mutate.rb
@@ -1,3 +1,5 @@
+# mutate.rb
+
 a = [1, 2, 3]
 
 # Example of a method definition that modifies its argument permanently.
@@ -9,3 +11,15 @@ end
 p "Before mutate method: #{a}"
 mutate(a)
 p "After mutate method: #{a}"
+
+a = [1, 2, 3]
+
+# Example of a method definition that does not modify the caller.
+
+def no_mutate(array)
+	array.last
+end
+
+p "Before no_mutate method: #{a}"
+no_mutate(a)
+p "After no_mutate method: #{a}"
