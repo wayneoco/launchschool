2cf89168f2ae8c02043d9c9c28c429dddbb377c4
wayneoco <wayne@wayneo.co>
2020-10-17 20:10:15 -0700

    Updated calculator.rb per Rubocop conventions

--- rb101/lesson_02/calculator.rb
+++ rb101/lesson_02/calculator.rb
@@ -1,19 +1,20 @@
-# calculator.rb
+# frozen_string_literal: false
 
-puts "What two numbers would you like to calculate?"
+puts 'What two numbers would you like to calculate?'
 numbers = gets.chomp.split
 
-puts "Thanks! Now, what mathematical operator would you like to use? You can choose from +, -, / or *."
+puts 'Thanks! Now, what mathematical operator would you like to use? You can choose from +, -, / or *.'
 operator = gets.chomp
 
-if operator == "+"
-	result = numbers[0].to_i + numbers[1].to_i
-	elsif operator == "-"
-		result = numbers[0].to_i - numbers[1].to_i
-	elsif operator == "/"
-		result = numbers[0].to_f / numbers[1].to_f
-	else
-		result = numbers[0].to_i * numbers[1].to_i
-end
+result =
+  if operator == '+'
+    numbers[0].to_i + numbers[1].to_i
+  elsif operator == '-'
+    numbers[0].to_i - numbers[1].to_i
+  elsif operator == '/'
+    numbers[0].to_f / numbers[1].to_i
+  else
+    numbers[0].to_i * numbers[1].to_i
+  end
 
-puts "#{numbers[0]} #{operator} #{numbers[1]} = #{result}."
+puts "#{numbers[0]} #{operator} #{numbers[1]} = #{result}"
