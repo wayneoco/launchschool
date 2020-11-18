4e5fdaf2db45a6e57c3eeff11b5e004eec0113eb
Wayne Olson <wayneolson@waynes-macbook-pro.local>
2020-11-18 22:12:31 +0000

    .

---
+++ Exercises/rb101-rb109_small_problems/easy_1/01.repeat_yourself.rb
@@ -0,0 +1,7 @@
+def repeat(string, num)
+	num.times do
+		puts string
+	end
+end
+
+repeat('Hello', 3)
---
+++ Exercises/rb101-rb109_small_problems/easy_1/03.list_of_digits.rb
@@ -0,0 +1,8 @@
+def digit_list(num)
+  digits = num.to_s.split('').to_i
+end
+  
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
