68b5101598c5b25094806e26487118da073afc5b
Wayne Olson <wayneolson@waynes-macbook-pro.local>
2021-08-31 18:27:21 +0000

    .

--- misc.rb
+++ misc.rb
@@ -1,17 +1,90 @@
-info = [["Launch School", "Online"], ["Wicked Witch", "The West"], ["Kermit the Frog", "Swamp"], ["Big Bird", "Sesame Street"], ["Ghostbusters", "Manhattan"]]
+WIN_RULES = {
+  'rock' => %w(scissors lizard),
+  'paper' => %w(rock spock),
+  'scissors' => %w(paper lizard),
+  'spock' => %w(scissors rock),
+  'lizard' => %w(spock paper)
+}
+VALID_CHOICES = WIN_RULES.keys
 
-mapped_info = (info.map do |item1|
-  item1.select do |item2|
-    item2.chars.each do |item3|
-      puts item3
-    end
+def prompt(message)
+  puts ("=> #{message}")
+end
+
+def player_chooses
+  prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+  gets.chomp
+end
+
+def valid_choice?(input)
+  return true if VALID_CHOICES.include?(input)
+  prompt("That's not a valid choice.")
+end
+
+def display_results(player, computer)
+  if WIN_RULES[player].include?(computer)
+    prompt("You win!")
+  elsif WIN_RULES[computer].include?(player)
+    prompt("You lose!")
+  else
+    prompt("It's a tie!")
   end
-end)
+end
 
-puts mapped_info == info
+def return_winner(player, computer)
+  if WIN_RULES[player].include?(computer)
+    return 'player'
+  elsif WIN_RULES[computer].include?(player)
+    return 'computer'
+  else
+    return 'tie'
+  end
+end
 
+def display_grand_winner(player_wins, computer_wins)
+  prompt("Congratulations! You're the grand winner!") if player_wins == 3
+  prompt("Sorry! Computer wins!") if computer_wins == 3
+end
 
-# item1 = each nested array within the array (e.g., ["Launch School", "Online"]
-# item2 = each item within the item1 array (e.g., "Launch School"]
-# item3 = each letter within the array of characters in item2 (e.g., "L")
+# Program start
 
+player_win_count = 0
+computer_win_count = 0
+
+loop do
+  prompt("Welcome to Rock, Paper, Scissors, Spock, Lizard!")
+  prompt("The first player to win 3 rounds is the grand winner!")
+  loop do
+    choice = ''
+    loop do
+      choice = player_chooses
+      break if valid_choice?(choice)
+    end
+    
+    computer_choice = WIN_RULES.keys.sample
+      
+    prompt("You chose: #{choice}. Computer chose: #{computer_choice}")
+    
+    display_results(choice, computer_choice)
+    
+    winner = return_winner(choice, computer_choice)
+    
+    player_win_count += 1 if winner == 'player'
+    computer_win_count += 1 if winner == 'computer'
+    
+    prompt("Your score: #{player_win_count}. Computer's score: #{computer_win_count}.")
+    print "\n"
+    
+    display_grand_winner(player_win_count, computer_win_count)
+    
+    break if player_win_count == 3 || computer_win_count == 3
+    
+  end
+  
+  prompt("Would you like to play again?")
+  answer = gets.chomp
+  
+  break if answer.downcase.start_with?('n')
+end
+
+prompt("Goodbye!")
